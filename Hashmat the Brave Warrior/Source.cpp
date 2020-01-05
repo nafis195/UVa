9dba3d80502ed70b414337ecb3418b53f0bd7d01
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-04 20:47:49 -0800

    Add files via upload

--- Hashmat the Brave Warrior/Source.cpp
+++ Hashmat the Brave Warrior/Source.cpp
@@ -1,1 +1,26 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codeforces - 10055 - Hashmat the Brave Warrior
+// Problem Statement - https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=996
+// Main.cpp
 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long int a, b, answer;
+
+	while (getchar() != EOF)
+	{
+		cin >> a >> b;
+		if (a > b)
+			answer = a - b;
+		else
+			answer = b - a;
+		cout << answer << endl;
+	}
+
+	return 0;
+}
