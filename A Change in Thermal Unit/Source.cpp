41e4e9b83502d8702954021ea8d249f81d0940b0
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-05 19:31:41 -0800

    Add files via upload

--- A Change in Thermal Unit/Source.cpp
+++ A Change in Thermal Unit/Source.cpp
@@ -1,6 +1,6 @@
 // Bismillahir Rahmanir Rahim
 // Nafis Chowdhury
-// Codeforces - 11984 - A Change in Thermal Unit
+// UVa - 11984 - A Change in Thermal Unit
 // Problem Statement - https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=3135
 // Main.cpp
 
