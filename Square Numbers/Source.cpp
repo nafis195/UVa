2954082eb2fcd622e38463d68173fc765d50a466
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-05 18:45:28 -0800

    Add files via upload

--- Square Numbers/Source.cpp
+++ Square Numbers/Source.cpp
@@ -1,1 +1,34 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codeforces - 11461 - Square Numbers
+// Problem Statement - https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=2456
+// Main.cpp
 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, count;
+	cin >> a >> b;
+
+	while (a != 0 && b!= 0)
+	{
+		count = 0;
+
+		for (int i = a; i <= b; i++)
+		{
+			if (i * i <= b)
+				count = count + 1;
+			else if (i * i > b)
+				break;
+		}
+
+		cout << count << endl;
+
+		cin >> a >> b;
+	}
+
+	return 0;
+}
